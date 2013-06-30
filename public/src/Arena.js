104f3f2c6cf4c79ae6f5d8ac6a0446a7f8f18a96
Arthur Jamain <arthur.jamain@gmail.com>
2013-06-30 22:27:28 +0200

    overhaul game/arena

--- public/src/Arena.js
+++ public/src/Arena.js
@@ -28,11 +28,10 @@ define([
      * @constructor
      * @this {Arena}
      */
-    initialize: function() {
+    initialize: function(opt) {
 
       this.ctx = this.el.getContext('2d');
-      this.toggle = 1;
-      this.initializeGrid();
+      this.game = opt.game;
 
     },
 
@@ -47,146 +46,74 @@ define([
         document.body.appendChild(this.el);
       }
     },
-
-    /**
-     * Creates the logical structure of the grid on which draw() is based.
-     */
-    initializeGrid: function() {
-
-      for (var i = 0 ; i < 3 ; i++) {
-        this.grid[i] = [];
-        for (var k = 0 ; k < 3 ; k++) {
-          this.grid[i][k] = new Grid(this, i, k);
-        }
-      }
-
-    },
     /**
      * Draws the actual grid on the canvas. Should also draw recorded moves as well, if any.
      */
-    draw: function() {
-
-      for (var i in this.grid) {
-        for (var k in this.grid[i]) {
-
-          this.grid[i][k].draw();
-
+    draw: function(grid) {
+      for (var i in grid) {
+        for (var k in grid[i]) {
+          this.drawGrid(grid[i][k]);
         }
       }
+    },
 
+    drawGrid: function(grid) {
+
+      var ctx = this.ctx;
+      ctx.beginPath();
+
+      /* /!\ WARNING, WALL AHEAD /!\ */
+      ctx.moveTo(grid.originX + Math.floor(grid.width / 3), grid.originY + this.padding);
+      ctx.lineTo(grid.originX + Math.floor(grid.width / 3), grid.originY + grid.height - this.padding);
+      ctx.moveTo(grid.originX + Math.floor(grid.width / 3) * 2, grid.originY + this.padding);
+      ctx.lineTo(grid.originX + Math.floor(grid.width / 3) * 2, grid.originY + grid.height - this.padding);
+      ctx.moveTo(grid.originX + this.padding, grid.originY + Math.floor(grid.width / 3));
+      ctx.lineTo(grid.originX + grid.width - this.padding, grid.originY + Math.floor(grid.width / 3));
+      ctx.moveTo(grid.originX + this.padding, grid.originY + Math.floor(grid.width / 3) * 2);
+      ctx.lineTo(grid.originX + grid.width - this.padding, grid.originY + Math.floor(grid.width / 3) * 2);
+
+      ctx.stroke();
+
+    },
+
+    drawSymbol: function(cell, symbol) {
+      var ctx = this.ctx;
+      ctx.beginPath();
+
+      if(symbol === -1) {
+
+        ctx.moveTo(cell.grid.originX + cell.originX + this.padding * 2, cell.grid.originY + cell.originY + this.padding * 2);
+        ctx.lineTo(cell.grid.originX + cell.originX + cell.width - this.padding * 2, cell.grid.originY + cell.originY + cell.height - this.padding * 2);
+        ctx.moveTo(cell.grid.originX + cell.originX + this.padding * 2, cell.grid.originY + cell.originY + cell.height - this.padding * 2);
+        ctx.lineTo(cell.grid.originX + cell.originX + cell.width - this.padding * 2, cell.grid.originY + cell.originY + this.padding * 2);
+
+      } else if (symbol === 1) {
+
+        ctx.arc(
+          cell.grid.originX + cell.originX + cell.width / 2,
+          cell.grid.originY + cell.originY + cell.height / 2,
+          30, 0, 2 * Math.PI, false
+        );
+
+      }
+      ctx.stroke();
     },
 
     onClick: function(e) {
       var point = e.touches ? e.touches[0] : e;
-      var xPos = Math.floor(point.offsetX / (this.el.width / 3));
-      var yPos = Math.floor(point.offsetY / (this.el.height / 3));
-      var grid = this.grid[xPos][yPos];
-      var symbol = 1;
+      var xGridPos = Math.floor(point.offsetX / (this.el.width / 3));
+      var yGridPos = Math.floor(point.offsetY / (this.el.height / 3));
+      var xCellPos = Math.floor(point.offsetX / (this.el.width / 9))%3;
+      var yCellPos = Math.floor(point.offsetY / (this.el.height / 9))%3;
 
-      grid.drawSymbol(point.offsetX, point.offsetY, this.toggle *= -1);
+      this.game.activate({
+        grid: [ xGridPos, yGridPos ],
+        cell: [ xCellPos, yCellPos ]
+      });
+
     }
 
   });
-
-  Cell = function(grid, x, y) {
-
-    this.grid = grid;
-    this.xPos = x;
-    this.yPos = y;
-    this.width = Math.floor(this.grid.width / 3);
-    this.height = Math.floor(this.grid.height / 3);
-    this.originX = this.xPos * this.width;
-    this.originY = this.yPos * this.height;
-
-  };
-  Cell.prototype.drawSymbol = function(symbol) {
-
-    var ctx = this.grid.arena.ctx;
-    ctx.beginPath();
-
-    if(symbol === -1) {
-      console.log(this.originX, this.originY);
-
-      ctx.moveTo(this.grid.originX + this.originX + this.grid.arena.padding * 2, this.grid.originY + this.originY + this.grid.arena.padding * 2);
-      ctx.lineTo(this.grid.originX + this.originX + this.width - this.grid.arena.padding * 2, this.grid.originY + this.originY + this.height - this.grid.arena.padding * 2);
-      ctx.moveTo(this.grid.originX + this.originX + this.grid.arena.padding * 2, this.grid.originY + this.originY + this.height - this.grid.arena.padding * 2);
-      ctx.lineTo(this.grid.originX + this.originX + this.width - this.grid.arena.padding * 2, this.grid.originY + this.originY + this.grid.arena.padding * 2);
-
-    } else if (symbol === 1) {
-
-      ctx.arc(
-        this.grid.originX + this.originX + this.width / 2,
-        this.grid.originY + this.originY + this.height / 2,
-        30, 0, 2 * Math.PI, false
-      );
-
-    }
-
-    ctx.stroke();
-  };
-
-  Grid = function (arena, x, y) {
-
-    this.arena = arena;
-    // Position in the main grid [3][3]
-    this.xPos = x;
-    this.yPos = y;
-    this.cells = [];
-    this.width = Math.floor(this.arena.el.width / 3);
-    this.height = Math.floor(this.arena.el.height / 3);
-    // Px origin of the grid inside the canvas
-    this.originX = this.xPos * this.width;
-    this.originY = this.yPos * this.height;
-
-    this.initCells();
-  };
-  Grid.prototype.initCells = function initCells () {
-    for (var i = 0 ; i < 3 ; i++) {
-      this.cells[i] = [];
-      for (var k = 0 ; k < 3 ; k++) {
-        this.cells[i][k] = new Cell(this, i, k);
-      }
-    }
-  };
-  Grid.prototype.draw = function draw () {
-
-    var ctx = this.arena.ctx;
-    ctx.beginPath();
-
-    /* /!\ WARNING, WALL AHEAD /!\ */
-    ctx.moveTo(this.originX + Math.floor(this.width / 3), this.originY + this.arena.padding);
-    ctx.lineTo(this.originX + Math.floor(this.width / 3), this.originY + this.height - this.arena.padding);
-    ctx.moveTo(this.originX + Math.floor(this.width / 3) * 2, this.originY + this.arena.padding);
-    ctx.lineTo(this.originX + Math.floor(this.width / 3) * 2, this.originY + this.height - this.arena.padding);
-    ctx.moveTo(this.originX + this.arena.padding, this.originY + Math.floor(this.width / 3));
-    ctx.lineTo(this.originX + this.width - this.arena.padding, this.originY + Math.floor(this.width / 3));
-    ctx.moveTo(this.originX + this.arena.padding, this.originY + Math.floor(this.width / 3) * 2);
-    ctx.lineTo(this.originX + this.width - this.arena.padding, this.originY + Math.floor(this.width / 3) * 2);
-
-    ctx.stroke();
-
-  };
-  Grid.prototype.drawSymbol = function drawSymbol (x, y, symbol) {
-
-    var point = this.getRelativePoint(x, y);
-    var cell = this.getCell(point.x, point.y);
-
-    cell.drawSymbol(symbol);
-
-  };
-  Grid.prototype.getRelativePoint = function getRelativePoint (x, y) {
-    return {
-      x: x - this.originX,
-      y: y - this.originY
-    };
-  };
-  Grid.prototype.getCell = function getCell (x, y) {
-    var cellX = Math.floor(x / (this.width / 3));
-    var cellY = Math.floor(y / (this.height / 3));
-    console.log('Cell', cellX, cellY);
-    return this.cells[cellX][cellY];
-
-  };
 
   return Arena;
 
--- public/src/Game.js
+++ public/src/Game.js
@@ -7,10 +7,12 @@ define([
   BaseObject,
   Arena
 ) {
-
-  return BaseObject.extend({
+  var Grid;
+  var Cell;
+  var Game = BaseObject.extend({
 
     players: [],
+    grid: [],
 
     initialize: function(opt) {
       console.info('Game Created !');
@@ -29,9 +31,61 @@ define([
 
       }
 
-      this.arena = new Arena();
+
+      this.arena = new Arena({
+        game: this
+      });
+
+      this.initializeGrid();
+
       this.arena.render();
-      this.arena.draw();
+      this.arena.draw(this.grid);
+    },
+
+    /**
+     * Creates the logical structure of the grid on which draw() is based.
+     */
+    initializeGrid: function() {
+
+      for (var i = 0 ; i < 3 ; i++) {
+        this.grid[i] = [];
+        for (var k = 0 ; k < 3 ; k++) {
+          this.grid[i][k] = new Grid(this.arena, i, k);
+        }
+      }
+    },
+
+    activate: function(coords) {
+
+      var g = coords.grid,
+          c = coords.cell,
+          grid = this.grid[g[0]][g[1]],
+          cell = grid.cells[c[0]][c[1]];
+
+      if((typeof this.getPlayableGrid() === 'undefined' ||
+        grid === this.getPlayableGrid() ) &&
+        !cell.activated ) {
+
+        if (!this.toggle) this.toggle = 1;
+
+        this.arena.drawSymbol(cell, this.toggle *= -1);
+        this.setPlayableGrid.apply(this, c);
+        cell.activated = true;
+
+      } else {
+
+        alert('LULZ NOPE');
+
+      }
+
+    },
+
+    setPlayableGrid: function(x, y) {
+      this.playableGrid = this.grid[x][y];
+    },
+
+    getPlayableGrid: function() {
+      return this.playableGrid;
     },
 
     onJoinPlz: function(data) {
@@ -55,4 +109,66 @@ define([
     }
 
   });
+
+
+
+
+
+
+  Cell = function(grid, x, y) {
+
+    this.grid = grid;
+    this.xPos = x;
+    this.yPos = y;
+    this.width = Math.floor(this.grid.width / 3);
+    this.height = Math.floor(this.grid.height / 3);
+    this.originX = this.xPos * this.width;
+    this.originY = this.yPos * this.height;
+
+  };
+
+  Grid = function (arena, x, y) {
+
+    this.arena = arena;
+    // Position in the main grid [3][3]
+    this.xPos = x;
+    this.yPos = y;
+    this.cells = [];
+    this.width = Math.floor(this.arena.el.width / 3);
+    this.height = Math.floor(this.arena.el.height / 3);
+    // Px origin of the grid inside the canvas
+    this.originX = this.xPos * this.width;
+    this.originY = this.yPos * this.height;
+
+    this.initCells();
+  };
+
+  _.extend(Grid.prototype, {
+
+    initCells: function () {
+      for (var i = 0 ; i < 3 ; i++) {
+        this.cells[i] = [];
+        for (var k = 0 ; k < 3 ; k++) {
+          this.cells[i][k] = new Cell(this, i, k);
+        }
+      }
+    },
+
+    getRelativePoint: function (x, y) {
+      return {
+        x: x - this.originX,
+        y: y - this.originY
+      };
+    },
+
+    getCell: function (x, y) {
+      var cellX = Math.floor(x / (this.width / 3));
+      var cellY = Math.floor(y / (this.height / 3));
+      console.log('Cell', cellX, cellY);
+      return this.cells[cellX][cellY];
+    }
+  });
+
+  return Game;
+
 });
